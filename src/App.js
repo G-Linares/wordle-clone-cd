a829732e360062b1f892d52cb18998f643e4afa4
G-Linares <gerrylinares@gmail.com>
2022-05-17 16:15:24 -0500

    advanced

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import { useState, createContext } from "react";
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./components/Words";
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
-      
+      <nav>
+        <h1>Wordle Cura Deuda</h1>
+      </nav>
+      <AppContext.Provider value={{board, setBoard}}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
