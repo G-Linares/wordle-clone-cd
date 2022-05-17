7862e10b3024e5dacb19e085172dce84a020c66d
G-Linares <gerrylinares@gmail.com>
2022-05-17 17:09:30 -0500

    muevo las funciones al main y las paso con provider

--- src/App.js
+++ src/App.js
@@ -10,12 +10,32 @@ function App() {
   const [board, setBoard] = useState(boardDefault);
   const [currAtempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
 
+  const onSelectLetter = (keyVal) => {
+    if(currAtempt.letterPos > 4) return;
+    const newBoard = [...board]
+    newBoard[currAtempt.attempt][currAtempt.letterPos] = keyVal;
+    setCurrAttempt({...currAtempt, letterPos: currAtempt.letterPos + 1});
+    setBoard(newBoard)
+  }
+
+  const onDelete = () => {
+    if(currAtempt.letterPos === 0) return;
+    const newBoard = [...board]
+    newBoard[currAtempt.attempt][currAtempt.letterPos - 1] = "";
+    setBoard(newBoard)
+    setCurrAttempt({...currAtempt, letterPos: currAtempt.letterPos - 1})
+  }
+  const onEnter = () => {
+    if(currAtempt.letterPos !== 5) return;
+    setCurrAttempt({ attempt: currAtempt.attempt + 1, letterPos:0})
+  }
+
   return (
     <div className="App">
       <nav>
         <h1>Wordle Cura Deuda</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard, currAtempt, setCurrAttempt }}>
+      <AppContext.Provider value={{ board, setBoard, currAtempt, setCurrAttempt, onSelectLetter, onDelete, onEnter }}>
         <div className="game">
           <Board />
           <Keyboard />
--- src/components/Key.js
+++ src/components/Key.js
@@ -2,18 +2,15 @@ import React, {useContext} from 'react'
 import { AppContext } from '../App'
 
 const Key = ({keyVal, bigKey}) => {
-    const {board, setBoard, currAtempt,setCurrAttempt} = useContext(AppContext)
+    const {onEnter, onDelete, onSelectLetter} = useContext(AppContext)
 
     const selectLetter = () => {
         if(keyVal === "ENTER"){
-            if(currAtempt.letterPos !== 5) return;
-            setCurrAttempt({ attempt: currAtempt.attempt + 1, letterPos:0})
+            onEnter()
+        } else if( keyVal === "DELETE"){
+            onDelete()
         } else {
-            if(currAtempt.letterPos > 4) return;
-            const newBoard = [...board]
-            newBoard[currAtempt.attempt][currAtempt.letterPos] = keyVal;
-            setCurrAttempt({...currAtempt, letterPos: currAtempt.letterPos + 1});
-            setBoard(newBoard)
+            onSelectLetter(keyVal)
         }
     }
 
